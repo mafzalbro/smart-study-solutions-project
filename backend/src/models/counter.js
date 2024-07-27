4cae5d4f3fe42073019fd7582a730c09afa58be0
mafzalbro <mafzalbro@gmail.com>
2024-07-27 10:23:19 +0500

    thanks to new changes

--- backend/src/models/counter.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const counterSchema = new Schema({
-  _id: { type: String, required: true },
-  sequenceValue: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('Counter', counterSchema);
--- backend/src/routes/whatsappRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/whatsappRoutes.js
-const express = require('express');
-const router = express.Router();
-const { sendWhatsAppMessage } = require('../controllers/whatsappController.js');
-const { auth } = require('../middlewares/auth');
-
-router.post('/send', auth, sendWhatsAppMessage);
-
-module.exports = router;
