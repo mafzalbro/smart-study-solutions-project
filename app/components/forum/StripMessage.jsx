be10195dc93ceb48ebdcb421024d4c53462d28c5
mafzalbro <mafzalbro@gmail.com>
2024-11-13 07:56:24 +0500

    forum red stripe modified

--- app/components/forum/StripMessage.jsx
+++ app/components/forum/StripMessage.jsx
@@ -13,26 +13,26 @@ const StripeMessage = () => {
 
   return (
     <div
-      className="fixed bottom-0 left-0 w-full bg-red-700 dark:bg-red-900 text-white py-0 md:py-2 px-4 md:px-6 z-50 flex justify-between items-center shadow-lg rounded-t-xl transition-opacity duration-500 ease-in-out opacity-100"
+      className="fixed bottom-0 left-0 w-full bg-red-700 dark:bg-red-900 text-white py-2 md:py-2 px-4 md:px-6 z-50 flex justify-between items-center shadow-lg rounded-t-xl transition-opacity duration-500 ease-in-out opacity-100"
       style={{ animation: "fadeIn 0.5s ease-in-out" }}
     >
-      <span className="text-sm md:text-base font-semibold">
-        Teacher's answers are excluded in the free version.
-      </span>
-      <div className="flex items-center">
+      <div className="flex flex-col sm:flex-row items-start sm:items-center">
+        <span className="text-sm md:text-base font-semibold">
+          Teacher's answers are excluded in the free version.
+        </span>
         <Link
           href="/pricing"
-          className="ml-4 text-blue-200 hover:text-blue-500 hover:underline font-medium transition-all duration-300"
+          className="sm:ml-4 text-blue-200 hover:text-blue-500 hover:underline font-medium transition-all duration-300 text-xs md:text-base"
         >
           Buy Membership
         </Link>
-        <button
-          onClick={handleClose}
-          className="ml-4 text-white hover:text-gray-200 transition-all duration-300"
-        >
-          <AiOutlineClose size={20} /> {/* Close Icon */}
-        </button>
       </div>
+      <button
+        onClick={handleClose}
+        className="ml-4 text-xs md:text-base text-white hover:text-gray-200 transition-all duration-300"
+      >
+        <AiOutlineClose size={20} /> {/* Close Icon */}
+      </button>
     </div>
   );
 };
